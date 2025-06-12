0adc81beb1557ab1305ec821e0c8f2790e2ddc8d
uc02 <upendrachauhan0206@gmail.com>
2025-06-12 14:01:53 +0530

    search query

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from './components/Search'
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
+import { updateSearchCount } from './appwrite';
 
 
 const API_BASE_URL = 'https://api.themoviedb.org/3'
@@ -49,6 +50,10 @@ const App = () => {
       }
 
       setMovieList(data.results || [])
+
+      if(query && data.result.length > 0){
+        await updateSearchCount(query, data.results[0])
+      }
     } catch (error) {
       console.error(`Error fetching movies: ${error}`)
       setErrorMessage('Error fetching movies. Please try again later.')
