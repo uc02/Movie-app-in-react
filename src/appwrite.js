0bab6406fb94d18a44e5a63f26ff968225136dfb
uc02 <upendrachauhan0206@gmail.com>
2025-06-12 14:32:58 +0530

    trending movie list

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,41 +1,52 @@
-import { Client, Databases, ID, Query } from "appwrite";
+import { Client, Databases, ID, Query } from 'appwrite'
 
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
-const ENDPOINT = import.meta.env.VITE_APPWRITE_ENDPOINT;
 
 const client = new Client()
- .setEndpoint(ENDPOINT)
- .setProject(PROJECT_ID)
+  .setEndpoint('https://fra.cloud.appwrite.io/v1')
+  .setProject(PROJECT_ID)
 
- const database = new Databases(client)
-
+const database = new Databases(client);
 
 export const updateSearchCount = async (searchTerm, movie) => {
-  //1. use Appwrite SDK to check if the search term exists in the database
-  try {
-    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
-Query.equal('searchTerm', searchTerm)
-    ])
-    
-    //2. if it does, update the count
-    if(result.documents.length > 0){
-      const doc = result.documents[0]
+  // 1. Use Appwrite SDK to check if the search term exists in the database
+ try {
+  const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+    Query.equal('searchTerm', searchTerm),
+  ])
 
-      await database.updateSearchCount(DATABASE_ID, COLLECTION_ID, doc.$id, {
-        count: doc.count + 1,
-      })
-      //3. if it doesn't, create a new document with the search term and counst as 1
-    }else{
-      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(),{
-        searchTerm,
-        count: 1,
-        movie_id: movie.id,
-        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-      })
-    }
-  } catch (error) {
-    console.error(error)
+  // 2. If it does, update the count
+  if(result.documents.length > 0) {
+   const doc = result.documents[0];
+
+   await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
+    count: doc.count + 1,
+   })
+  // 3. If it doesn't, create a new document with the search term and count as 1
+  } else {
+   await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+    searchTerm,
+    count: 1,
+    movie_id: movie.id,
+    poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+   })
   }
+ } catch (error) {
+  console.error(error);
+ }
+}
+
+export const getTrendingMovies = async () => {
+ try {
+  const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+    Query.limit(5),
+    Query.orderDesc("count")
+  ])
+ 
+  return result.documents;
+ } catch (error) {
+  console.error(error);
+ }
 }
